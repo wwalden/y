744cd5f4053bd91807c3fec37f31efe3932ab294
Arnaud Jallut <a.jallut@bee-buzziness.com>
2023-02-14 16:47:09 +0100

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next();
+});
+
+
+app.use(express.static("public"))
+
+
+app.get('/about', (req, res) => {
+  res.send({'message': 'hello world'})
+})
+
+const PORT = process.env.PORT || 4200
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
+
